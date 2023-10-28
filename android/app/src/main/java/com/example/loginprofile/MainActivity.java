06f8dd275b29f3865fb399dbbf708399c9b82a58
ayshasahana <ayshashana656@gmail.com>
2023-10-28 15:44:12 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/loginprofile/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.loginprofile;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home: loginprofile(),));
+
+}
+class loginprofile extends StatefulWidget {
+  const loginprofile({super.key});
+
+  @override
+  State<loginprofile> createState() => _loginprofileState();
+}
+
+class _loginprofileState extends State<loginprofile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
